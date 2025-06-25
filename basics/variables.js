134f77337a56045bcac53616ecbe86b4ca10d2b2
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-06-25 12:15:11 +0000

    variables done

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 212545;
+let accountEmail = "rudra@gmail.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+// accountId = 2; This is not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "25212545"
+accountCity = "Delhi"
+let accountState;
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
