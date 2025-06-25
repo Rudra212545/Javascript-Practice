73127174ed26d405a95f15faa72674b4cdfd5adf
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-06-25 12:31:41 +0000

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all JS code as newer versions
+
+// alert(3+3) we are using nodejs not browser
+
+let name = "Rudra" //String
+let age = 18 // number 
+let isLoggedIn = true //boolean=>true/false\
+let state = null
+//Null => standalone value
+//undefined => value is not assign 
+// Symbol => unique
+
+
+// Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
