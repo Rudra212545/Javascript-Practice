aa3cb1024f6dc8cc8b7a75a3cac2d47c06623889
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-06-25 16:01:38 +0000

    add comparisons for undefined values and comment out null checks

---
+++ basics/comparisons.js
@@ -0,0 +1,21 @@
+// Null - avoid
+// console.log(null >=0);
+// console.log(null ==0);
+// console.log(null ===0);
+// console.log(null <=0);
+// console.log(null !=0);
+// console.log(null >0);
+// console.log(null < 0);
+
+// console.log("2" == 2);
+// // Strict check 
+// console.log("2" === 2);
+
+// undefined - avoid
+console.log(undefined >=0);
+console.log(undefined ==0);
+console.log(undefined ===0);
+console.log(undefined <=0);
+console.log(undefined !=0);
+console.log(undefined >0);
+console.log(undefined < 0);
