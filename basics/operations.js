efd9c76b4f4d5ae4c8aef72640b20864076d4bd9
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-06-25 13:04:02 +0000

    operations done

---
+++ basics/operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+// Basic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// String concatination 
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1+str2
+// console.log(str3);
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// If string is at first then all converted to the string 
+// console.log("1"+2+2);
+// if string is at last then first do the operations and then concatinate the string
+// console.log(1+2+"2");
+// console.log(1+2+2+"4"+"5");
+
+// In this the example , the value of x assign to y is shows 100 and then increments and now x becomes 101 and now ++x chnages the value of z to 102
+let x = 100;
+let y = x++;
+let z = ++x;
+console.log(`PostFix: ${y}`)
+console.log(`PreFix: ${z}`)
+
+
+
+
