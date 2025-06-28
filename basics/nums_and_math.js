e83abbd5d717d4b51a9909c58f07a36e99c62cdc
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-06-28 08:04:33 +0000

    add number manipulation examples and random number generation

---
+++ basics/nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+// const anotherBalance = 25.786542;
+// console.log(anotherBalance.toPrecision(3)); // Note: toPrecision takes only one argument
+
+const hunderes = 1000000;
+// console.log(hunderes.toLocaleString('en-IN')); // Converts number to a string with locale-specific formatting
+
+
+// Maths 
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-5)); // Absolute value
+// console.log(Math.round(4.6)); // Rounds to nearest integer
+// console.log(Math.ceil(4.1)); // Rounds up to nearest integer ceiling
+// console.log(Math.floor(4.9)); // Rounds down to nearest integer floor 
+// console.log(Math.min(1, 2, 3, 4, 5)); // Minimum value
+// console.log(Math.max(1, 2, 3, 4, 5)); // Maximum value 
+
+console.log(Math.random()); // Generates a random number between 0 and 1
+console.log((Math.random() * 10) +1 ); // Generates a random number between 0 and 10 
+console.log(Math.floor((Math.random() * 10) +1) ); // Generates a random number between 0 and 10 
+
+const min = 10;
+const max = 20;
+
+console.log (Math.floor(Math.random() * (max - min +1)) +min); // Generates a random number between min and max
+
+
+
+
+
+
