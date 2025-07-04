3fa5a564f1a233c94a507717a4c299b589d1aebf
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-07-04 09:58:18 +0000

    add examples of function scopes and variable usage

--- basics3/scopes.js
+++ basics3/scopes.js
@@ -12,3 +12,45 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "rudra";
+
+    function two(){
+        const website = "youtube.com";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+one();
+
+if (true) {
+    const username = "rudra";
+    if(username === "rudra") {
+        const website = "youtube.com";
+        console.log(username + " " + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// +++++++++++++ intresting +++++++++++++++
+
+console.log(addOne(10)); // 11
+function addOne(value){
+    return value +1;
+}
+
+// Mini hoisting example
+// Function declaration is hoisted
+// Function expression is not hoisted
+const  addTwo = function(value){
+    return value + 2;
+}
+addTwo(10); // 12
+
