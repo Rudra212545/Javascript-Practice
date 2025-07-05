53540b141c4b8af835e00df88191d2d4615d0bb7
Rudra Parmeshwar <rudra25parmeshwar@gmail.com>
2025-07-05 08:39:53 +0000

    add switch case example for month display

---
+++ control_flow/two.js
@@ -0,0 +1,54 @@
+// Switch case
+// switch(key){
+//     case value:
+//         // code to execute if key matches value
+//         break;
+//     case anotherValue:
+//         // code to execute if key matches anotherValue
+//         break;
+//     default:
+//         // code to execute if key does not match any case
+//         break;
+// }
+
+const month = 3;
+switch(month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+}
